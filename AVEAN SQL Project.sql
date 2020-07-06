370fdd4a2ccd5ea80e319baba7b43a35bdeb9b2e
drashtivora11 <62382739+drashtivora11@users.noreply.github.com>
2020-07-06 20:14:10 +0530

    Add files via upload

---
+++ AVEAN SQL Project.sql
@@ -0,0 +1,11 @@
+INSERT INTO Supplier (
+                         Supplier_Id,
+                         Supplier_Name,
+                         Supplier_Region,
+                         Supplier_Address,
+                         Supplier_Contact,
+                         Supplier_Email,
+                         Ingredient_Id,
+                         Ingredient_Name
+                     )
+             Values(1,'a','b','c',963,'d',1,'Peanut');
